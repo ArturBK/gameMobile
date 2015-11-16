0a479661947f028675da5b1b988e2ddffbe77abc
= <artur.koefender@ilegra.com>
2015-11-16 18:47:44 -0200

    Segundo commit

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class Inimigo : MonoBehaviour {
 
-	// Use this for initialization
+	[SerializeField]private int vida = 10;
+
 	void Start () {
 		NavMeshAgent agente = GetComponent<NavMeshAgent> ();
 		GameObject fim = GameObject.Find ("FimDoCaminho");
 		agente.SetDestination (fim.transform.position);
+	}
+
+	public void recebeDano(int pontosDeDano){
+		vida -= pontosDeDano;
+		if (vida <= 0) {
+			Destroy(this.gameObject);
+		}
 	}
 }
--- Assets/Scripts/Missel.cs
+++ Assets/Scripts/Missel.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class Missel : MonoBehaviour {
 
 	private float velocidade = 10;
+	private float tempoDeVida = 10;
+	[SerializeField] private int pontosDeDano = 1;
 	GameObject alvo;
 
 	void Start()
 	{
 		alvo = GameObject.Find ("Inimigo");
+		CleanGarbage ();
 	}
 	
 	void Update () {
 		Anda();
-		AlteraDirecao ();
+		if (alvo != null) {
+			AlteraDirecao ();
+		}
 	}
 
 	private void Anda(){
@@ -30,6 +35,18 @@ public class Missel : MonoBehaviour {
 		Vector3 direcaoDoAlvo = posicaoDoAlvo - posicaoAtual;
 
 		transform.rotation = Quaternion.LookRotation (direcaoDoAlvo);
+	}
+
+	private void CleanGarbage(){
+		Destroy (this.gameObject, 10);
+	}
+
+	void OnTriggerEnter(Collider elemento){
+		if (elemento.CompareTag ("Inimigo")) {
+			Destroy (this.gameObject);
+			Inimigo inimigo = elemento.GetComponent<Inimigo>();
+			inimigo.recebeDano(pontosDeDano);
+		}
 	}
 
 }
